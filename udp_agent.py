956cf8c88fad46e03dbb2fff4b2b67ff928224a2
ronishamai <ronishamai@mail.tau.ac.il>
2024-06-24 01:02:59 +0300

    Add files for UDP Pinger project

--- udp_agent.py
+++ udp_agent.py
@@ -1,53 +1,58 @@
 from socket import *
 import sys
 import struct
+import time
 
 ### GLOBAL VARIABLES ###
-host = 'localhost'
-port = 1337
+host = "localhost"
+port = 1336
+
 
 def init_connection():
     global host
     global port
-    
-    agent = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # create an (IPV4, UDP) socket object 
+
+    agent = socket(AF_INET, SOCK_DGRAM)  # create an (IPV4, UDP) socket object
     agent.bind((host, port))
     return agent
 
-def message_manipulator(data): 
-    agent_enc = struct.pack('!B', 1)
+
+def message_manipulator(data):
+    agent_enc = struct.pack("!B", 1)
     return agent_enc + data[1:]
+
 
 def interact(agent):
     data, pinger_addr = agent.recvfrom(1405)
     message = message_manipulator(data)
     agent.sendto(message, pinger_addr)
 
+
 def main():
     try:
         agent = init_connection()
     except:
-        print('couldnt connect')
+        print("couldnt connect")
         exit(1)
     try:
         while True:
             interact(agent)
     except:
-        print('interacted enough')
+        print("interacted enough")
         agent.close()
         exit(1)
-    agent.close()
+
 
 if __name__ == "__main__":
-    if ((len(sys.argv) < 1) or (len(sys.argv) > 3)):
+    if (len(sys.argv) < 1) or (len(sys.argv) > 3):
         print("Error! Too many variables \ few variables")
         sys.exit(1)
 
     try:
-        port_index = sys.argv.index('-p') + 1
+        port_index = sys.argv.index("-p") + 1
         port = int(sys.argv[port_index])
 
     except:
         pass
- 
+
     main()
--- udp_pinger.py
+++ udp_pinger.py
@@ -13,67 +13,81 @@ timeout = 1
 ID = 0
 fails = 0
 
+
 def message_builder(data):
     global ID
-    ID = ID + 1 
-    encoded_message = data.encode('utf-8')
-    pinger_enc = struct.pack('!B', 0)
-    ID_enc = struct.pack('!I', ID)
+    ID = ID + 1
+    encoded_message = data.encode("utf-8")
+    pinger_enc = struct.pack("!B", 0)
+    ID_enc = struct.pack("!I", ID)
     return pinger_enc + ID_enc + encoded_message
 
+
 def print_ok_message(t, rcv_msg, IP):
-        #we will use the global value instead of decoding the message, bc we already made sure we got back the correct message using validate data
-        global ID 
-        data_len = len(rcv_msg[5:])
-        print(f"{data_len} bytes from {IP}: seq={ID} rtt={t} ms")
+    # we will use the global value instead of decoding the message, bc we already made sure we got back the correct message using validate data
+    global ID
+    data_len = len(rcv_msg[5:])
+    print(f"{data_len} bytes from {IP}: seq={ID} rtt={t} ms")
+
 
 def print_bad_message(msg_ID):
     print(f"request timeout for icmp_seq {msg_ID}")
+
 
 def print_summary():
     global count
     global fails
     global agent_IP
     print(f"--- {agent_IP} statistics ---")
-    print(f"{count} packets transmitted, {fails} packets received, {(fails/count):.2f}% packet loss")
+    print(
+        f"{count} packets transmitted, {count - fails} packets received, {(fails/count):.2f}% packet loss"
+    )
+
 
 def validate_data(rcv_msg, send_msg):
-    return ((rcv_msg[0] == 1) and (rcv_msg[1:] == send_msg[1:]))
+    return (rcv_msg[0] == 1) and (rcv_msg[1:] == send_msg[1:])
+
 
 def init_connection():
-    pinger = socket(socket.AF_INET, socket.SOCK_DGRAM) # create an (IPV4, UDP) socket object 
+    pinger = socket(AF_INET, SOCK_DGRAM)  # create an (IPV4, UDP) socket object
     pinger.settimeout(timeout)
     return pinger
+
 
 def ping(pinger, agent_address, data):
     global fails
     global count
-    
-    #send message
+    global ID
+
+    # send message
     send_msg = message_builder(data)
     pinger.sendto(send_msg, agent_address)
     start_time = time.perf_counter()
-    
-    #receive message
+
+    # receive message
     time_window = start_time + timeout
-    while (time.perf_counter() < time_window):
-        rcv_msg, agent = pinger.recvfrom(1405)
-        end_time = time.perf_counter()
-        if validate_data(rcv_msg, send_msg):
-            rtt = (end_time -  start_time) * 1000
-            print_ok_message(rtt, rcv_msg, agent)
-            return 
-        
-    print_bad_message(count)
+    while time.perf_counter() < time_window:
+        try:
+            rcv_msg, agent = pinger.recvfrom(1405)
+            end_time = time.perf_counter()
+            if validate_data(rcv_msg, send_msg):
+                rtt = (end_time - start_time) * 1000
+                print_ok_message(rtt, rcv_msg, agent[0])
+                return
+        except TimeoutError:
+            break
+
+    print_bad_message(ID)
     fails = fails + 1
-    
+
+
 def main():
     try:
         pinger = init_connection()
     except:
         print("could not connect")
         exit(1)
-    try:    
+    try:
         data = size * "."
         for i in range(count):
             ping(pinger, agent_address, data)
@@ -86,23 +100,22 @@ def main():
 
 
 if __name__ == "__main__":
-    if ((len(sys.argv) < 2) or (len(sys.argv) > 10)):
+    if (len(sys.argv) < 2) or (len(sys.argv) > 10):
         print("Error! Too many variables \ few variables")
         sys.exit(1)
 
     agent_IP = sys.argv[1]
-    # agent_IP = '127.0.0.1'
     agent_address = (agent_IP, agent_port)
 
     try:
-        port_index = sys.argv.index('-p') + 1
+        port_index = sys.argv.index("-p") + 1
         agent_port = int(sys.argv[port_index])
         agent_address = (agent_IP, agent_port)
     except:
         pass
-    
+
     try:
-        size_index = sys.argv.index('-s') + 1
+        size_index = sys.argv.index("-s") + 1
         size = int(sys.argv[size_index])
         if size > 1400:
             print("Error! Size is bigger than 1400")
@@ -111,15 +124,15 @@ if __name__ == "__main__":
         pass
 
     try:
-        count_index = sys.argv.index('-c') + 1
+        count_index = sys.argv.index("-c") + 1
         count = int(sys.argv[count_index])
     except:
         pass
 
     try:
-        timeout_index = sys.argv.index('-t') + 1
-        timeout = float(sys.argv[timeout_index])/1000
+        timeout_index = sys.argv.index("-t") + 1
+        timeout = float(sys.argv[timeout_index]) / 1000
     except:
         pass
-    
+
     main()
